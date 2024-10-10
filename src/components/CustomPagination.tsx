c3711550d0801f4f579478a02e016d140e18619e
MauDavalos10 <mauricio.davalos@hivemapper.com>
2024-10-10 00:40:07 -0500

    Responsive Pagination

--- src/components/CustomPagination.tsx
+++ src/components/CustomPagination.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Pagination, Stack } from "@mui/material";
+import { Pagination, Stack, useMediaQuery, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/store";
 import { setPageIndex } from "../redux/slices/pageIndexSlice";
 import { searchMovies } from "../redux/slices/movieSlice";
 
 const CustomPagination = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const dispatch = useDispatch<AppDispatch>();
   const { totalResults } = useSelector((state: RootState) => state.movies);
   const query = useSelector((state: RootState) => state.query);
@@ -25,7 +27,7 @@ const CustomPagination = () => {
         page={pageIndex}
         onChange={handlePageChange}
         sx={paginationSx}
-        size={"large"}
+        size={isMobile ? "small" : "large"}
         shape={"rounded"}
       />
     </Stack>
